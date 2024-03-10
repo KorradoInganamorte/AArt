aea43bec1ed4a3f290f38559ea243281b54b9384
Korrado Inganamorte <petristomin1812@gmail.com>
2024-03-10 19:47:12 +0300

    optimization

--- components/film/SearchFilmBar.tsx
+++ components/film/SearchFilmBar.tsx
@@ -66,8 +66,8 @@ const SearchFilmBar = ({ showDropdown, setShowDropdown, setSearchQuery }: Props)
           <div ref={dropdownListContainerRef} className={`${showDropdown ? "relative" : "hidden"} z-[2] w-[100%] mt-[.2rem] py-[1.8rem] bg-gray-search_dropdown rounded-[1rem]`}>
             {animes?.data.slice(0, 8).filter(anime => anime.attributes.title.toLowerCase().includes(value.toLowerCase())).map(anime => (
               <div onClick={() => handleClickDropdownList(anime)} key={anime.id} className="flex justify-between items-center w-[100%] hover:bg-gray pl-[2rem] pr-[2.6rem] my-[.4rem] last:mb-0  ease-in-out transition-colors">
-                <p className={`${robotoMedium} text-xl text-white py-[.4rem]`}>{anime.attributes.title}</p>
-                <img className="w-[1.7rem] h-[1.6rem]" src="/images/Search_white.svg" alt="search icon" />
+                <p className={`max-w-[57.5rem] ${robotoMedium} text-xl text-white py-[.4rem]`}>{anime.attributes.title}</p>
+                <img className="w-[1.9rem] h-[1.9rem]" src="/images/Search_white.svg" alt="search icon" />
               </div>
             ))}
           </div>
